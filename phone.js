5efc19903e7c43b5ff2ff85749ace254993a02c3
SubiNair <subirnair@gmail.com>
2018-01-24 17:33:42 -0700

    updated jscript and css

--- phone.js
+++ phone.js
@@ -1,23 +1,23 @@
-(document).ready(function(){
-	("#content_dial").show();
-	("#content_contacts").hide();
-	("#content_add").hide();
+$(document).ready(function(){
+	$("#content_dial").show();
+	$("#content_contacts").hide();
+	$("#content_add").hide();
 });
 
-("#dial").click(function(){
-	("#content_dial").show();
-	("#content_contacts").hide();
-	("#content_add").hide();
+$("#dial").click(function(){
+	$("#content_dial").show();
+	$("#content_contacts").hide();
+	$("#content_add").hide();
 });
 
-("#contacts").click(function(){
-	("#content_contacts").show();
-	("#content_dial").hide();
-	("#content_add").hide();
+$("#contacts").click(function(){
+	$("#content_contacts").show();
+	$("#content_dial").hide();
+	$("#content_add").hide();
 });
 
-("#add").click(function(){
-	("#content_add").show();
-	("#content_dial").hide();
-	("#content_contacts").hide();
+$("#add").click(function(){
+	$("#content_add").show();
+	$("#content_dial").hide();
+	$("#content_contacts").hide();
 });
